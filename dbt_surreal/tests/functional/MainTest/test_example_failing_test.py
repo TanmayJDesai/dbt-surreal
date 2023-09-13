9eb44b77ff6275882ffedcab42ab462374718797
TanmayJDesai <desai.j.tanmay@gmail.com>
2023-09-13 13:02:16 -0700

    changed testfile a little, removed filler

--- dbt_surreal/tests/functional/MainTest/test_example_failing_test.py
+++ dbt_surreal/tests/functional/MainTest/test_example_failing_test.py
@@ -2,11 +2,8 @@ import pytest
 from dbt.tests.util import run_dbt
 from dbt_surreal.tests.functional.MainTest.fixtures import my_new_seed_csv, my_new_model_sql, my_new_model_yml
 
-
-# class must begin with 'Test'
 class TestExample:
 
-    # configuration in dbt_project.yml
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {
@@ -14,23 +11,19 @@ class TestExample:
             "models": {"+materialized": "view"},
         }
 
-    # everything that goes in the "seeds" directory
     @pytest.fixture(scope="class")
     def seeds(self):
         return {
             "my_seed.csv": my_seed_csv,
         }
 
-    # everything that goes in the "models" directory
     @pytest.fixture(scope="class")
     def models(self):
         return {
-            "my_new_model.sql": my_new_model_sql,  # Use the updated model SQL
-            "my_new_model.yml": my_new_model_yml,  # Use the updated model YAML
+            "my_new_model.sql": my_new_model_sql,
+            "my_new_model.yml": my_new_model_yml,
         }
 
-    # The actual sequence of dbt commands and assertions
-    # pytest will take care of all "setup" + "teardown"
     def test_run_seed_test(self, project):
 
         # seed seeds
@@ -42,12 +35,10 @@ class TestExample:
         # test tests
         results = run_dbt(["test"], expect_pass=False)  # expect failing test
         assert len(results) == 2
-        # validate that the results include one pass and one failure
         result_statuses = sorted(r.status for r in results)
         assert result_statuses == ["fail", "pass"]
 
     @pytest.mark.xfail
     def test_build(self, project):
         """Expect a failing test"""
-        # do it all
         results = run_dbt(["build"])
